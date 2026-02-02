1e1a3c121ab8ae3b0f07bddc547ccfed14334d0d
Ivanov_Serhiy <s.ivanov@flex-solution.com>
2026-02-02 13:41:06 +0200

    Fix app component css

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranscriptComponent } from './component/transcript/transcript.component';
+import { AiAnswerComponent } from "./component/answer/answer.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [TranscriptComponent],
+  imports: [TranscriptComponent, AiAnswerComponent],
 })
 export class App {}
