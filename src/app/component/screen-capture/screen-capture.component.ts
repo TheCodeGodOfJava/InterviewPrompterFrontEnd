407317da93683d019a993f497ff00b6bdf6eca94
Ivanov_Serhiy <s.ivanov@flex-solution.com>
2026-02-22 10:36:15 +0200

    Add screen capture component

---
+++ src/app/component/screen-capture/screen-capture.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-screen-capture',
+  imports: [],
+  templateUrl: './screen-capture.html',
+  styleUrl: './screen-capture.scss',
+})
+export class ScreenCapture {
+
+}
